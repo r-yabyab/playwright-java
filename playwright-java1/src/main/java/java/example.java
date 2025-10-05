f7777f03e9c91803b7ef18fe39d5f5735c2af369
Scruffy pc <cayabyabrr@yahoo.com>
2025-10-04 20:11:04 -0500

    Boilerplate working

---
+++ playwright-java1/src/main/java/java/example.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import com.microsoft.playwright.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class example {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("https://playwright.dev");
+            System.out.println(page.title());
+        }
+    }
+}
