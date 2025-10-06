8e2a3212402aa9c91260c711cd24ba0f391e8e93
Scruffy pc <cayabyabrr@yahoo.com>
2025-10-06 14:46:35 -0500

    Simple auth test

--- playwright-java1/src/test/java/AuthenticationTests.java
+++ playwright-java1/src/test/java/AuthenticationTests.java
@@ -2,6 +2,8 @@ import com.microsoft.playwright.*;
 import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 //npx playwright codegen demo.playwright.dev/todomvc
 //npx playwright codegen saucedemo.com
 
@@ -35,12 +37,13 @@ public class AuthenticationTests {
     @DisplayName("Invalid Login")
     void test() {
         page.navigate("https://www.saucedemo.com/");
-        page.locator("[data-test=\"username\"]").click();
         page.locator("[data-test=\"username\"]").fill("standard_user1");
-        page.locator("[data-test=\"username\"]").press("Tab");
         page.locator("[data-test=\"password\"]").fill("secret_sauce");
         page.locator("[data-test=\"login-button\"]").click();
-        page.locator("[data-test=\"error\"]").click();
-        String invalidLoginMessage = "Epic sadface: Username and password do not match any user in this service";
+
+        String actualMessage = page.locator("[data-test=\"error\"]").textContent().trim();
+        String expectedMessage = "Epic sadface: Username and password do not match any user in this service";
+
+        assertEquals(expectedMessage, actualMessage);
     }
 }
